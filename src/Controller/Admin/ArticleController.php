1a115a73d1e01140e9177c1c7d27982b81d95d95
Julien Anest <julien.anest@evogue.fr>
2020-04-03 17:25:35 +0200

    Articles

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -4,8 +4,15 @@
 namespace App\Controller\Admin;
 
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -32,11 +39,10 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/edition")
+     * @Route("/edition/{id}", defaults={"id": null}, requirements={"id": "\d+"})
      */
-    public function edit()
+    public function edit(Request $request, EntityManagerInterface $manager, $id)
     {
-        $user = $this->getUser();
         /*
          * Intégrer le formulaire pour l'enregistrement d'un article
          * Validation : tous les champs obligatoires
@@ -46,9 +52,101 @@ class ArticleController extends AbstractController
          * Adapter la page pour la modification :
          * - pas de modification de la date de publication ni de l'auteur
          */
+        $originalImage = null;
+
+        if (is_null($id)) { // création
+            $article = new Article();
+            $article->setAuthor($this->getUser());
+        } else { // modification
+            $article = $manager->find(Article::class, $id);
+
+            if (is_null($article)) {
+                throw new NotFoundHttpException();
+            }
+
+            if (!is_null($article->getImage())) {
+                // nom du fichier venant de la bdd
+                $originalImage = $article->getImage();
+
+                // le champ de formulaire attend un objet File
+                $article->setImage(
+                    new File($this->getParameter('upload_dir') . $article->getImage())
+                );
+            }
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                /** @var UploadedFile|null $image */
+                $image = $article->getImage();
+
+                // si une image a été uploadée
+                if (!is_null($image)) {
+                    // nom sous lequel on va enregistrer l'image
+                    $filename = uniqid() . '.' . $image->guessExtension();
+
+                    // déplacement de l'image uploadée
+                    $image->move(
+                        // dans quel répertoire
+                        // cf config/services.yaml
+                        $this->getParameter('upload_dir'),
+                        // nom du fichier
+                        $filename
+                    );
+
+                    // pour enregistrer le nom du fichier en bdd
+                    $article->setImage($filename);
+
+                    // suppression de l'ancienne image en modification s'il y en a une
+                    if (!is_null($originalImage)) {
+                        unlink($this->getParameter('upload_dir') . $originalImage);
+                    }
+                } else {
+                    // pour la modification, sans upload,
+                    // on remet le nom de l'image venant de la bdd
+                    $article->setImage($originalImage);
+                }
+
+                $manager->persist($article);
+                $manager->flush();
+
+                $this->addFlash('success', "L'article est enregistré");
+
+                return $this->redirectToRoute('app_admin_article_index');
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+            }
+        }
 
         return $this->render(
-            'admin/article/edit.html.twig'
+            'admin/article/edit.html.twig',
+            [
+                'form' => $form->createView(),
+                'original_image' => $originalImage
+            ]
         );
+    }
+
+    /**
+     * @Route("/suppression/{id}", requirements={"id": "\d+"})
+     */
+    public function delete(EntityManagerInterface $manager, Article $article)
+    {
+        // suppression de l'image si l'article en a une
+        if (!is_null($article->getImage())) {
+            unlink($this->getParameter('upload_dir') . $article->getImage());
+        }
+
+        // suppression en bdd
+        $manager->remove($article);
+        $manager->flush();
+
+        $this->addFlash('success', "L'article est supprimé");
+
+        return $this->redirectToRoute('app_admin_article_index');
     }
 }
